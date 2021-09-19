dda3460bd5d762ca3d291bae557dbd002cf4ee41
ashcan1 <ashki444@gmail.com>
2021-09-19 12:44:21 +0100

    first

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Printhalf
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of rows:\t");
+            int rows = int.Parse(Console.ReadLine());
+            int i, j;
+            for (i = 1; i <= rows; ++i)
+            {
+               for (j = 1 ; j <= i; ++j)
+                {
+                Console.Write("* ");
+                
+            }
+            Console.WriteLine();
+        }
+    }
+}
+}
+

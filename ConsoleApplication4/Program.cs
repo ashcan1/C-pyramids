0180d42e94dce17b42fe8bbac62c5fb37427b21b
ashcan1 <ashki444@gmail.com>
2021-09-20 07:47:46 +0100

    second

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -12,14 +12,40 @@ namespace Printhalf
             int i, j;
             for (i = 1; i <= rows; ++i)
             {
-               for (j = 1 ; j <= i; ++j)
+                for (j = 1; j <= i; ++j)
                 {
-                Console.Write("* ");
-                
+
+
+
+
+
+                    Console.Write("* ");
+
+                }
+
+                Console.WriteLine("\n");
             }
-            Console.WriteLine();
+   
+
+
+
+            for (i = rows; i >= 1; i--)
+            {
+                for (j = 1; j <= i; j++)
+                {
+
+
+
+                    Console.Write("* ");
+
+                }
+
+                Console.WriteLine();
+            }
+
+
         }
     }
 }
-}
+    
 

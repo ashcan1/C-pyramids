111a8a2f4c9d7659a5eb267e74b776b8d8a1abb2
ashcan1 <ashki444@gmail.com>
2021-09-21 12:10:36 +0100

    fourth shape

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -21,6 +21,7 @@ namespace Printhalf
 
                 Console.WriteLine();
             }
+
             Console.WriteLine();
 
             for (i = rows; i >= 1; i--)
@@ -33,24 +34,49 @@ namespace Printhalf
 
                 Console.WriteLine();
             }
+
             Console.WriteLine();
+
             
-            
+
             for (i = rows; i >= 1; i--)
             {
-            
-                for (j = 1; j <= i; ++j)
-                { 
-                    Console.Write("* ");
+                for (j = 1; j <= rows - i; ++j)
+                {
+                    Console.Write("  ");
+                }
 
+                for (j = 1; j <= i; ++j)
+                {
+                    Console.Write("* ");
                 }
 
                 Console.WriteLine();
             }
-        }
-        
-        }
-         }
+            Console.WriteLine();
+            
+            
+            for (i = 1 ; i <= rows; ++i)
+            {
+                for (j = 1; j <= rows - i; ++j)
+                {
+                    Console.Write("  ");
+                }
 
-    
+                for (j = 1; j <= i; ++j)
+                {
+                    Console.Write("* ");
+                }
 
+                Console.WriteLine();
+            }
+
+            
+
+
+
+        }
+    }
+
+
+}
